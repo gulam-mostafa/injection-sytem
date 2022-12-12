b70e3cebb2ba7e47cd711ac656611a65ad5fec4a
unknown <gulam.mostafa209@gmail.com>
2022-12-12 13:35:59 +0600

    input data

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,23 +6,38 @@ import Options from '../Pages/Options';
 
 
 const Home = () => {
-    const [totals, setTotal] = useState(0)
-    const [minus, seMinus] = useState(0)
+      const [cycleTime, setCycleTime] = useState(0)
+      const [preformSize, setPreformSize] = useState(0)
+      const [currentPreforms, setCurrentPreforms] = useState(0)
+      const [activeCavity, setActiveCavity] = useState(0)
+      const [remainsTime, setRemainsTime] = useState(0)
+      const [perCartons, setPerCartons] = useState(0)
+      const [wastage, setWastage] = useState(0)
+      const [underPreforms, setUnderPreforms] = useState(0)
+      const [perContainer, setPerContainer] = useState(0)
+      const [perShift, setPerShift] = useState(0)
     const add = (event) => {
         event.preventDefault()
         const form = event.target;
-        const a = form.a.value;
-        const b = form.b.value;
-        const aI = parseFloat(a)
-        const bI = parseFloat(b)
+        const preformSizes = form.preformssize.value;
+        const cycleTimes = form.cycleTimes.value;
+        const currentPreforms = form.currentPreforms.value;
+        const activeCavity = form.activeCavity.value;
+        const remainsTime = form.remainsTime.value;
+        const perCartons = form.perCartons.value;
+        const perCase = form.perCase.value;
+        const wastage = form.wastage.value;
+        const underPreforms = form.underPreforms.value;
+        const perContainer = form.perContainer.value;
+        const perShift = form.perShift.value;
+
+        // console.log(preformSizes)
+
+        console.log(perCartons, preformSizes,  cycleTimes, currentPreforms, activeCavity, remainsTime, perCase, wastage,underPreforms,perContainer,perShift)
 
 
-        const total = a - b
-        const minus = b + a
-        const totaloi = a + totals
-        // console.log(total)
-        setTotal(total)
-        seMinus(minus)
+
+
     }
 
 
@@ -30,7 +45,7 @@ const Home = () => {
         <div className='  my-4 lg:mx-4 mx-2 '>
             <Options></Options>
             <h2 className='underline underline-offset-4 decoration-wavy  mb-8 decoration-black -600 text-center text-xl font-bold my-4 text-blue-600 '>Preforms calculator page</h2>
-            <form className="flex flex-col  gap-1">
+            <form onSubmit={add} className="flex flex-col  gap-1">
                 <div className=' grid lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4  gap-1 lg:gap-4  place-items-stretch'>
                     <div className=' flex justify-between rounded bg-gray-100 px-2 border border-purple-300 '>
                         {/* preform size */}
@@ -42,7 +57,12 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Type Size" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input
+                                id='preformssize'
+                                name='preformssize'
+                                type="text"
+                                placeholder="Type Size"
+                                className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1      w-10 text-end '> gms</p>
                         </div>
                     </div>
@@ -56,7 +76,8 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Type Cycle Times" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input name='cycleTimes'
+                                type="text" placeholder="Type Cycle Times" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1     w-10 text-end '>Sec</p>
                         </div>
                     </div>
@@ -70,7 +91,8 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Type Current Preforms"  className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input name='currentPreforms'
+                                type="text" placeholder="Type Current Preforms" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1     w-10 text-end '> Ps</p>
                         </div>
                     </div>
@@ -84,23 +106,28 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Active Cavity" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input name='activeCavity'
+                                type="text" placeholder="Active Cavity" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1     w-10  text-end '> Ps</p>
                         </div>
                     </div>
-                    <div className=' flex justify-between rounded bg-violet-100 px-2  border border-purple-300'>
-                        {/* Remaining Time */}
-                        <div className="mb- w-4/5 ">
-                            <Label
-                                className='text-lg font-bold'
-                                htmlFor="email2"
-                                value="Remaining Time "
-                            />
+                    <div>
+                        <div className=' flex justify-between rounded bg-violet-100 px-2  border border-purple-300'>
+                            {/* Remaining Time */}
+                            <div className="mb- w-4/5 ">
+                                <Label
+                                    className='text-lg font-bold'
+                                    htmlFor="email2"
+                                    value="Remaining Time "
+                                />
+                            </div>
+                            <div className='flex w-1/ justify-end '>
+                                <input name='remainsTime'
+                                    type="text" placeholder="Remain Minute" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                                <p className='ml-1     w-10 text-end '> Min.</p>
+                            </div>
                         </div>
-                        <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Remain Minute" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
-                            <p className='ml-1     w-10 text-end '> Min.</p>
-                        </div>
+
                     </div>
                     <div className=' flex justify-between rounded bg-blue-100 px-2  border border-purple-300 '>
                         {/* Per cartons */}
@@ -112,7 +139,8 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Type per cartons"  className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input name='perCartons'
+                                type="text" placeholder="Type per cartons" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1     w-10 text-end '> ps</p>
                         </div>
                     </div>
@@ -126,7 +154,8 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Type per case" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input name='perCase'
+                                type="text" placeholder="Type per case" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1     w-10 text-end '> ps</p>
                         </div>
                     </div>
@@ -140,7 +169,8 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Type Wastage in kg" className="input text-orange-500 text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input name='wastage'
+                                type="text" placeholder="Type Wastage in kg" className="input text-orange-500 text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1     w-10 text-end '> Kgs</p>
                         </div>
                     </div>
@@ -154,12 +184,13 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Type cartons" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input name='underPreforms'
+                                type="text" placeholder="Type cartons" className="input text-orange-500  text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1     w-10 text-end '> crt.</p>
                         </div>
                     </div>
                     <div className=' flex justify-between rounded bg-orange-100 px-2  border border-purple-300 '>
-                        {/* Per container */}
+                        {/* Per / container /*/}
                         <div className="mb- w-4/5 ">
                             <Label
                                 className='text-lg font-bold'
@@ -168,7 +199,8 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" placeholder="Cartons" className="input text-orange-500 text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input name='perContainer'
+                                type="text" placeholder="Cartons" className="input text-orange-500 text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1     w-10 text-end '> crt.</p>
                         </div>
                     </div>
@@ -182,17 +214,19 @@ const Home = () => {
                             />
                         </div>
                         <div className='flex w-1/ justify-end '>
-                            <input type="text" defaultValue={8} className="input text-orange-500 text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
+                            <input
+                                name='perShift' type="text" defaultValue={8} className="input text-orange-500 text-lg font-bold input-bordered rounded-lg bg-red- input-sm w-full -w-xs" />
                             <p className='ml-1   text-xs items-center  w-10 text-end '> hours</p>
                         </div>
                     </div>
-                    
                 </div>
+                <p className='text-xs text-center'> <span className='text-red-500'>*</span> Remaining time is, When you calculate before finish shift</p>
 
                 <Button className='mt-4 lg:w-1/2 w-full mx-auto' type="submit">
                     Calculate now
                 </Button>
             </form>
+
         </div>
     );
 };
